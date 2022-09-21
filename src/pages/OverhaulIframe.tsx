7df56d75c5c5dc556ea703335379b899daa71e5e
Anton Zinchenko <12981061+Antoniozinchenko@users.noreply.github.com>
2022-09-21 17:32:11 +0300

    add iframe params

--- src/pages/OverhaulIframe.tsx
+++ src/pages/OverhaulIframe.tsx
@@ -3,5 +3,10 @@ import { EnvContext } from "../services/EnvContext";
 
 export const OverhaulIframe = () => {
   const envContext = useContext(EnvContext);
-  return <iframe src={envContext.url} title='overhaul-iframe' id='overhaul-iframe' />
+  return <iframe
+    src={envContext.url}
+    title='overhaul-iframe'
+    id='overhaul-iframe'
+    sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
+  />
 }
